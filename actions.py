120c485c9f0d220d7e341bb60467c5a3f91ab105
Beverly Bates <bbates@bastiansolutions.com>
2024-04-04 17:17:11 -0500

    Completed Lesson2 of roguelike tutorial

--- actions.py
+++ actions.py
@@ -34,7 +34,7 @@ class MovementAction(Action):
         dest_y = entity.y + self.dy
 
         if not engine.game_map.in_bounds(dest_x, dest_y):
-            return #Destination is out of bounds.
+            return #Destination is out of bounds.git pugit 
         if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
             return #Destination is blocked by a tile
             
