53dd034e43c0e3b297eec814b6162e6607fa0f5e
Beverly Bates <bbates@bastiansolutions.com>
2024-04-09 11:07:50 -0500

    First commit

--- tile_types.py
+++ tile_types.py
@@ -35,3 +35,6 @@ floor = new_tile(
 wall = new_tile(
     walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
 )
+ironOre = new_tile(
+    walkable=True, transparent=False, dark=(ord('+'), (100,117,128), (0, 0, 0))
+)
